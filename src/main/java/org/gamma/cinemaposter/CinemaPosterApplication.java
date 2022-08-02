ca3143bb990d09dabb2cd9a83ce0977c3af723c7
Maxim Fomov <maksimfomov26@gmail.com>
2022-08-02 18:03:04 +0300

    init: Created project

---
+++ src/main/java/org/gamma/cinemaposter/CinemaPosterApplication.java
@@ -0,0 +1,13 @@
+package org.gamma.cinemaposter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaPosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaPosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gamma/cinemaposter/CinemaPosterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gamma.cinemaposter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaPosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
